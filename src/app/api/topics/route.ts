4bed512cd29756a0bed45da510e1e64c10a7176d
Ella-Truong <ellatruong95@gmail.com>
2026-07-05 23:59:20 -0700

    feat: update GET route

--- src/app/api/topics/route.ts
+++ src/app/api/topics/route.ts
@@ -3,6 +3,7 @@
  * Because no query parameters, request body, and special headers
  * So GET route doesn't need to inspect the request
  */
+
 import { NextResponse } from "next/server";
 import { TopicService } from "@/backend/services/topic.service";
 

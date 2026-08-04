198dd915f522783b0d379ece3a82e3b3557c2454
leowomen <ellatruong95@gmail.com>
2026-08-04 12:44:12 -0700

    refactor(e2e): separate local and CI env configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,6 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -36,7 +33,9 @@ export default defineConfig({
    * npm run test:e2e -> Start Next.js -> wait until localhost:300 is ready -> run tests
    */
   webServer: {
-    command: "npm run build:e2e && npm run start:e2e",
+    command: process.env.CI
+      ? "npm run build:e2e && npm run start:e2e"
+      : "npm run build:e2e:local && npm run start:e2e:local",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
